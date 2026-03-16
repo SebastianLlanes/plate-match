82d0a483e16f08c144233585adcf7d04bc66f51c
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2026-03-16 09:54:45 -0300

    feat: move firebase config to env variables

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4CG-odGVYZVn3cTsA4pW-GxP7ysQkYc0",
-  authDomain: "plate-match-8f9a7.firebaseapp.com",
-  projectId: "plate-match-8f9a7",
-  storageBucket: "plate-match-8f9a7.firebasestorage.app",
-  messagingSenderId: "566197008452",
-  appId: "1:566197008452:web:9654c55bdb6f1788032411",
-  measurementId: "G-1NJ0PP0YF1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  
 };
 
 export const app = initializeApp(firebaseConfig);
